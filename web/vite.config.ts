8cae07d154363a54edadf9ed0e3035b0f14b1841
vultr <vultr740@gmail.com>
2025-10-31 15:04:45 +0800

    feat: api server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        // target: 'http://localhost:8080',
+        target: 'https://nofx-2vs8.onrender.com',
         changeOrigin: true,
       },
     },
